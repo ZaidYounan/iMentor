b30a8216f1e090e0530446353a15a7f5d8e5056a
ZaidYounan <zaid.younan@gmail.com>
2021-05-18 20:54:02 +1000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+  root "projects#index"
+  get "/index", to: "projects#index"
+  resources :projects
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
