8d300e08d30c204eb7500a5bbb2395db03deffb0
ZaidYounan <zaid.younan@gmail.com>
2021-05-22 00:55:49 +1000

    Add comment model and conntroller, add comment routes, edit views to add comment form and display comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   root "projects#index"
   get "/index", to: "projects#index"
-  resources :projects
+  resources :projects do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_070245) do
+ActiveRecord::Schema.define(version: 2021_05_21_142421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "project_name"
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_070245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "projects"
 end
