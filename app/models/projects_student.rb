5f19af4efd940f52f328a4834307bae15974b3e9
ZaidYounan <zaid.younan@gmail.com>
2021-05-27 23:12:53 +1000

    Fixed bug with enrolling only in one course at a time

--- app/models/projects_student.rb
+++ app/models/projects_student.rb
@@ -1,6 +1,5 @@
 class ProjectsStudent < ApplicationRecord
     self.table_name = "projects_students"
-    validates :student_id, uniqueness: true
     belongs_to :student
     belongs_to :project
 end
